aa8aa9f2798bbdc225f10ad0087579a032d34792
wendallR <wendallar125@gmail.com>
2022-10-26 12:16:27 -0400

    Add client download

--- src/Client/Client.java
+++ src/Client/Client.java
@@ -2,12 +2,16 @@ package Client;
 
 
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
 import java.nio.channels.SocketChannel;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class Client {
 
@@ -41,7 +45,7 @@ public class Client {
         switch (command[0]){
             case "i":
                 buffer = initialize(command);
-                save();
+                saveInitialized();
                 return "File set, ready to upload";
             case "u":
                 upload();
@@ -89,7 +93,7 @@ public class Client {
     }
 
     private String download() throws IOException {
-        String instruction = "d";
+        String instruction = "d" + file.name;
         byte[] b = new byte[2048];
         b = instruction.getBytes();
         ByteBuffer buffer = ByteBuffer.wrap(b);
@@ -100,8 +104,27 @@ public class Client {
         char reply = getServerCode(sc);;
         System.out.println("Can be downloaded: " + reply);
         if (reply == 'y'){
-            sc.shutdownOutput();
-            sc.close();
+            System.out.println("The request was accepted");
+            Files.createDirectories(Paths.get("./downloaded"));
+            //make sure to set the "append" flag to true
+            BufferedWriter bw = new BufferedWriter(new FileWriter("./downloaded/"+file.name, true));
+            ByteBuffer data = ByteBuffer.allocate(1024);
+            int bytesRead;
+
+            while ((bytesRead = sc.read(data)) != -1) {
+                //before reading from buffer, flip buffer
+                //("limit" set to current position, "position" set to zero)
+                data.flip();
+                byte[] a = new byte[bytesRead];
+                //copy bytes from buffer to array
+                //(all bytes between "position" and "limit" are copied)
+                data.get(a);
+                String serverMessage = new String(a);
+                bw.write(serverMessage);
+                data.clear();
+            }
+            bw.close();
+
             return "Downloaded";
         }
         else {
@@ -146,7 +169,7 @@ public class Client {
         sc.close();
     }
 
-    private void save() throws IOException {
+    private void saveInitialized() throws IOException {
         final String content = file.content;
         String path = "./initialized/" + file.name;
         File file = new File(String.valueOf(path));
